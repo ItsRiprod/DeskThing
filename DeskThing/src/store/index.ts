693c3bb0b32c25abd3a69d7500c1db1f6e63e062
ItsRiprod <riprodgames@gmail.com>
2024-10-13 12:05:03 -0700

    Removed old client

--- DeskThing/src/store/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AppStore } from './appStore'
-export { default as ControlStore } from './controlStore'
-export { default as ManifestStore } from './manifestStore'
-export type { ServerManifest } from './manifestStore'
